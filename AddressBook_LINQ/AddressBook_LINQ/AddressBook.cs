d2440bbbd3cba0f7bad1ce3b56247fc53d7328b3
Rajangane <pratiksharajangane7@gmail.com>
2022-01-13 17:39:26 +0530

    [Pratiksha] Creating Address Book Table

---
+++ AddressBook_LINQ/AddressBook_LINQ/AddressBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_LINQ
+{
+    public class AddressBook
+    {
+        public void CreateAddressBookDataTable()
+        {
+            //DataTable 
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("AddressBookName", typeof(string));
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("Zip", typeof(int));
+            dataTable.Columns.Add("PhoneNumber", typeof(long));
+            dataTable.Columns.Add("Email", typeof(string));
+            dataTable.Columns.Add("AddressBookType", typeof(string));
+        }
+    }
+}
--- AddressBook_LINQ/AddressBook_LINQ/Program.cs
+++ AddressBook_LINQ/AddressBook_LINQ/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("*********** Welcome To Address Book *****************");
+//Console.WriteLine("*********** Welcome To Address Book *****************");
+using AddressBook_LINQ;
+AddressBook address = new AddressBook();
+address.CreateAddressBookDataTable();
